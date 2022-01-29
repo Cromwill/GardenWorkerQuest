70d4e29bcf7208ea832b394fc25b828bf7fd24d9
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2022-01-29 23:26:37 +0300

    Ice breaker first iteration

---
+++ Assets/Scripts/IceBreaker.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceBreaker : MonoBehaviour
+{
+    [SerializeField] private InputActionAdapter _input;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void MoveActivator()
+    {
+
+    }
+}
