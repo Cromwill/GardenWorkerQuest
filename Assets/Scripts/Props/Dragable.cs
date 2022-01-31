a37af4edfc568ab7193edc19949406e3a99541fd
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2022-01-31 12:30:35 +0300

    throwing stick complete

--- Assets/Scripts/Props/Dragable.cs
+++ Assets/Scripts/Props/Dragable.cs
@@ -8,60 +8,31 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(Rigidbody))]
 public class Dragable : Interactable
 {
-    private float _flySpeed = 0.008f;
-    private bool _isDestinatioReached;
+    [SerializeField] private LayerMask _mask;
+
     private Rigidbody _rigidbody;
-    private bool _isDraged;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    private IEnumerator FlyTo(Vector3 Destination, GameObject obj)
-    {
-        while (obj.transform.position != Destination && _isDraged)
-        {
-            obj.transform.position = Vector3.MoveTowards(obj.transform.position, Destination, _flySpeed);
-
-            yield return null;
-        }
-
-        if(_isDraged == true)
-            _isDestinatioReached = true;
-    }
-
     private void OnMouseDrag()
     {
-        _isDraged = true;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, 100, _mask))
+                transform.position = new Vector3(hitInfo.point.x, 1f, hitInfo.point.z);
+    }
+
+    private void OnMouseDown()
+    {
         _rigidbody.isKinematic = true;
-
-        Vector3 positionOnScreen = Camera.main.WorldToScreenPoint(transform.position);
-        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, positionOnScreen.z));
-        transform.position = mouseWorldPosition;
-
-        if (_isDestinatioReached == false)
-            StartCoroutine(FlyTo(new Vector3(-0.5f, 1.5f, -0.9f), transform.GetChild(0).gameObject));
     }
 
     private void OnMouseUp()
     {
         _rigidbody.isKinematic = false;
-        _isDraged = false;
-        ResetPosition();
-    }
-
-    public void ResetPosition()
-    {
-        Transform child = transform.GetChild(0);
-        transform.DetachChildren();
-        transform.position = child.transform.position;
-        child.SetParent(transform);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.TryGetComponent(out Shredder shredder))
-            _isDraged = false;
     }
 }
